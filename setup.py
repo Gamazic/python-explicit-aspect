decc822f87e350076e1bed7b7a21f66a6851e111
Gamazic <nikita.nov.ru@gmail.com>
2022-03-26 23:30:03 +0300

    change name, author, author email

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='aspect',
-    packages=find_packages(include=['aspect']),
+    name='python-explicit-aspect',
+    packages=['aspect'],
     version='0.1.0',
     description='Aspect Oriented Programming python library',
-    author='Gamazic, nikita.nov.ru@gmail.com',
+    author='Gamazic = "Nikita Burov"',
+    author_email = 'nikita.nov.ru@gmail.com',
     license='MIT',
 )
