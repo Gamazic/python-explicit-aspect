98d92bb996404455010fae31cc60325b74157d37
Gamazic <nikita.nov.ru@gmail.com>
2022-03-26 23:00:55 +0300

    add setup.py file for building library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aspect',
+    packages=find_packages(include=['aspect']),
+    version='0.1.0',
+    description='Aspect Oriented Programming python library',
+    author='Gamazic, nikita.nov.ru@gmail.com',
+    license='MIT',
+)
